a1c57d3caf6a73fb99ab0a45df3226b9d8cc79ec
Cheng Jinbao <cjbi@outlook.com>
2025-03-30 11:15:30 +0800

    配置路由地址

--- src/main/java/tech/wetech/flexmodel/infrastructrue/StaticResources.java
+++ src/main/java/tech/wetech/flexmodel/infrastructrue/StaticResources.java
@@ -12,37 +12,10 @@ public class StaticResources {
   void installRoute(@Observes StartupEvent startupEvent, Router router) {
     router.route()
       .path("/fm-ui/*")
-      .handler(handler -> handler.reroute("/webjars/flexmodel-ui/"));
-    router.route()
-      .path("/assets/:resource")
       .handler(ctx -> {
-        String resource = ctx.pathParam("resource");
-        ctx.reroute("/webjars/flexmodel-ui/assets/" + resource);
+        String resource = ctx.pathParam("*");
+        ctx.reroute("/webjars/flexmodel-ui/" + resource);
       });
-    router.route()
-      .path("/swagger-ui/:resource")
-      .handler(ctx -> {
-        String resource = ctx.pathParam("resource");
-        ctx.reroute("/webjars/flexmodel-ui/swagger-ui/" + resource);
-      });
-    router.route()
-      .path("/graphiql/:resource")
-      .handler(ctx -> {
-        String resource = ctx.pathParam("resource");
-        ctx.reroute("/webjars/flexmodel-ui/graphiql/" + resource);
-      });
-    router.route()
-      .path("/rapi-doc/:resource")
-      .handler(ctx -> {
-        String resource = ctx.pathParam("resource");
-        ctx.reroute("/webjars/flexmodel-ui/rapi-doc/" + resource);
-      });
-    router.route()
-      .path("/logo.png")
-      .handler(handler -> handler.reroute("/webjars/flexmodel-ui/logo.png"));
-    router.route()
-      .path("/favicon.ico")
-      .handler(handler -> handler.reroute("/webjars/flexmodel-ui/favicon.ico"));
   }
 
 }
