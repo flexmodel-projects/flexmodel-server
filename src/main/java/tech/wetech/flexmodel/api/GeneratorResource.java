0c55a5b2c8122c0c26494faa048fd294cd6c5576
Cheng Jinbao <cjbi@outlook.com>
2025-04-11 13:53:01 +0800

    feat: 新增出码的逻辑

--- src/main/java/tech/wetech/flexmodel/api/GeneratorResource.java
+++ src/main/java/tech/wetech/flexmodel/api/GeneratorResource.java
@@ -1,14 +1,18 @@
 package tech.wetech.flexmodel.api;
 
-import jakarta.ws.rs.Consumes;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.StreamingOutput;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import tech.wetech.flexmodel.Entity;
+import tech.wetech.flexmodel.SchemaObject;
+import tech.wetech.flexmodel.codegen.*;
+import tech.wetech.flexmodel.domain.model.modeling.ModelService;
 
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -18,33 +22,86 @@ import static tech.wetech.flexmodel.api.Resources.ROOT_PATH;
  * @author cjbi
  */
 @Tag(name = "【Flexmodel】生成器", description = "生成器管理")
-@Path(ROOT_PATH + "/generator")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
+@Path(ROOT_PATH + "/codegen")
 public class GeneratorResource {
 
+  @Inject
+  ModelService modelService;
+
   @GET
-  @Path("/code.zip")
-  public Response generate() {
+  @Path("/{datasource}_{model}.zip")
+  public Response generate(@PathParam("datasource") String datasourceName, @PathParam("model") String modelName) {
     StreamingOutput stream = output -> {
       try (ZipOutputStream zipOut = new ZipOutputStream(output)) {
 
-        // 模拟第一个文件
-        zipOut.putNextEntry(new ZipEntry("hello.txt"));
-        zipOut.write("Hello from Quarkus!\n".getBytes());
+        String dir = String.format("%s_%s/tech/wetech/flexmodel/", datasourceName, modelName);
+
+        zipOut.putNextEntry(new ZipEntry(String.format("%s_%s/", datasourceName, modelName)));
         zipOut.closeEntry();
 
-        // 模拟第二个文件
-        zipOut.putNextEntry(new ZipEntry("data/info.txt"));
-        zipOut.write("Dynamic ZIP with directory structure.\n".getBytes());
+        zipOut.putNextEntry(new ZipEntry(String.format("%s_%s/tech/", datasourceName, modelName)));
         zipOut.closeEntry();
+
+        zipOut.putNextEntry(new ZipEntry(String.format("%s_%s/tech/wetech/", datasourceName, modelName)));
+        zipOut.closeEntry();
+
+        zipOut.putNextEntry(new ZipEntry(String.format("%s_%s/tech/wetech/flexmodel/", datasourceName, modelName)));
+        zipOut.closeEntry();
+
+        zipOut.putNextEntry(new ZipEntry(String.format("%s_%s/tech/wetech/flexmodel/entity/", datasourceName, modelName)));
+        zipOut.closeEntry();
+
+        zipOut.putNextEntry(new ZipEntry(String.format("%s_%s/tech/wetech/flexmodel/dao/", datasourceName, modelName)));
+        zipOut.closeEntry();
+
+        zipOut.putNextEntry(new ZipEntry(String.format("%s_%s/tech/wetech/flexmodel/dsl/", datasourceName, modelName)));
+        zipOut.closeEntry();
+
+        zipOut.putNextEntry(new ZipEntry(String.format("%s_%s/tech/wetech/flexmodel/enumeration/", datasourceName, modelName)));
+        zipOut.closeEntry();
+
+        List<SchemaObject> models = modelService.findModels(datasourceName);
+
+        for (SchemaObject model : models) {
+          if (model instanceof Entity entity) {
+            ModelClass modelClass = GenerationTool.buildModelClass("com.example", datasourceName, entity);
+
+            GenerationContext context = new GenerationContext();
+            context.setModelClass(modelClass);
+            context.putVariable("rootPackage", "com.example");
+
+
+            zipOut.putNextEntry(new ZipEntry(dir + "/entity/" + modelClass.getShortClassName() + ".java"));
+            zipOut.write(new PojoGenerator().generate(context).getBytes());
+            zipOut.closeEntry();
+
+            zipOut.putNextEntry(new ZipEntry(dir + "/dsl/" + modelClass.getShortClassName() + "DSL.java"));
+            zipOut.write(new DSLGenerator().generate(context).getBytes());
+            zipOut.closeEntry();
+
+            zipOut.putNextEntry(new ZipEntry(dir + "/dao/" + modelClass.getShortClassName() + "DAO.java"));
+            zipOut.write(new DaoGenerator().generate(context).getBytes());
+            zipOut.closeEntry();
+          } else if (model instanceof tech.wetech.flexmodel.Enum anEnum) {
+            EnumClass enumClass = GenerationTool.buildEnumClass("com.example", datasourceName, anEnum);
+            GenerationContext context = new GenerationContext();
+            context.setEnumClass(enumClass);
+            context.putVariable("rootPackage", "com.example");
+
+            zipOut.putNextEntry(new ZipEntry(dir + "/enumeration/" + enumClass.getShortClassName() + ".java"));
+            zipOut.write(new EnumGenerator().generate(context).getBytes());
+            zipOut.closeEntry();
+
+          }
+        }
+
 
         zipOut.finish();
       }
     };
 
     return Response.ok(stream)
-      .header("Content-Disposition", "attachment; filename=\"code.zip\"")
+      .header("Content-Disposition", String.format("attachment; filename=\"%s_%s.zip\"", datasourceName, modelName))
       .build();
   }
 
